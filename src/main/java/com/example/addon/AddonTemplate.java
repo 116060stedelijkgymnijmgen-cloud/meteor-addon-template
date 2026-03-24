103ebacd791eb9cb429e4a72302d9b4d24fd5824
116060stedelijkgymnijmgen-cloud <116060stedelijkgymnijmgen@gmail.com>
2026-03-24 20:40:52 +0100

    Register INFO module during initialization

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -21,6 +21,8 @@ public class AddonTemplate extends MeteorAddon {
     @Override
     public void onInitialize() {
         LOG.info("Initializing Meteor Addon Template");
+        
+        Hud.get().register(EnabledModulesHUD.INFO);
 
         // Modules
         Modules.get().add(new ModuleExample());
